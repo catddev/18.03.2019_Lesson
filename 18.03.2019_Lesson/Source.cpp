5f8722eb9d6b11a6d2e9fa37ce8afb4d070692fc
айкеновад <@204_13>
2019-03-18 09:11:14 +0600

    Добавьте файлы проекта.

---
+++ 18.03.2019_Lesson/Source.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <iomanip>
+#include<math.h>
+#include<ctime>
+#include<cstring>
+#include<fstream>
+
+using namespace std;
+
+struct student {
+	char name[30];
+	char surname[30];
+	int age;
+	int grades[5];
+};
+
+
+
+int main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+
+	while (true)
+	{
+		cout << "Задача № ";
+		cin >> tn; cin.get();
+
+		switch (tn)
+		{
+		case 1:
+		{
+			student s1;
+		}
+		break;
+		case 2:
+		{
+
+		}
+		break;
+		case 3:
+		{
+
+		}
+		break;
+		case 4:
+		{
+
+		}
+		break;
+		case 5:
+		{
+
+		}
+		break;
+		case 6:
+		{
+
+		}
+		break;
+		case 7:
+		{
+
+		}
+		break;
+		case 8:
+		{
+
+		}
+		break;
+		case 9:
+		{
+
+		}
+		break;
+		case 10:
+		{
+
+		}
+		break;
+		default:
+			cout << "нет такой задачи" << endl << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
